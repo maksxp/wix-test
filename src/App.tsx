a56361b7a10527fe584958b9d91263682c974ab9
maksym <maksym.murza@gmail.com>
2025-11-27 17:09:28 +0200

    height is 100vh

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div class="bg-green-100 min-h-200 flex items-center justify-center">
+    <div class="bg-green-100 min-h-screen flex items-center justify-center">
       <p class="text-3xl font-bold underline">
         TEST WIX
       </p>
